ead0b4a1be828f44a8bfd201293e1b4cb9b770c7
Ariel-41 <92064977+Ariel-41@users.noreply.github.com>
2025-03-12 11:25:47 -0300

    Delete api/config directory

--- api/config/database.php
+++
@@ -1,8 +0,0 @@
-<?php
-return [
-    'host' => getenv('DB_HOST') ?? 'localhost',
-    'dbname' => getenv('DB_NAME') ?? 'modulo_contable',
-    'user' => getenv('DB_USER') ?? 'root',
-    'password' => getenv('DB_PASSWORD') ?? '',
-    'charset' => 'utf8mb4',
-];
